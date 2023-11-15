873b9664c38a82ad7655e6fa8cfd00c44f26706e
myhedinv <myhedin.vuciterna@student.uni-pr.edu>
2023-11-15 17:19:58 +0100

    Update server.js
    added if checks for certain commands,
    added functionality for read permission,
    added functionality for exit command,
    added checks for command params,
    added a check for elevated permissions for 1 user.

--- server.js
+++ server.js
@@ -51,6 +51,45 @@ function commandHandler(commandsArray, socket) {
   const req = commands[0];
   const isAuthenticated = authenticatedClients.includes(socket);
 
+  if (req === "/read") {
+    if (!checkLengthOfCommandArray(commandsArray)) {
+        socket.write("Invalid arguments.");
+        return;
+    }
+
+    const filename = commands[1];
+    if (fs.existsSync(filename)) {
+        const content = fs.readFileSync(filename, 'utf8');
+        socket.write(`The file contains: ${content}`);
+    } else {
+        socket.write("File does not exist.");
+    }
+} else if (req === "/write" || req === "/execute") { // combined in 1 if statement because both require elevated perms
+    if (!isAuthenticated || !checkLengthOfCommandArray(commandsArray)) {
+        socket.write(!isAuthenticated ? "Unauthorized." : "Invalid arguments.");
+        return;
+    }
+
+    if (req === "/write") {
+     //add logic for writing content onto files.
+    } else { // /execute
+       //add logic for executing a file. good luck.
+    }
+} else if (req === "/exit") {
+    socket.write("/exit"); // sends it back just to close the loop back in client
+    socket.end();
+} else if (req === "/list") {
+    //add logic to list all clients in the socket servers
+} else if (req === "/password") {
+    if (!checkLengthOfCommandArray(commandsArray)) {
+        socket.write("Invalid arguments.");
+        return;
+    }
+    //add logic to check if password is correct, don't forget error checking, don't give too much info(security issue?)
+} else {
+    socket.write('400: Invalid command');
+}
+return;
 }
 
 function checkLengthOfCommandArray(commandsArray) {
